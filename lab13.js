aac241a4f51ebc9057bd5e03b32abd65dcb1566d
Ivan Arrieta <ivanarrieta243@gmail.com>
2026-01-20 16:17:37 -0600

    Se cambio el nombre del boton y la alerta y se cambiaron las capturas

--- lab13.js
+++ lab13.js
@@ -14,6 +14,6 @@ const appendAlert = (message, type) => {
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
+    appendAlert('Alerta, alguien presionó el botón', 'success')
   })
 }
